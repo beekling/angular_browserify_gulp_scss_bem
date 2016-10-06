e77243102a69a9b7e7c9b634fbc653749c48df51
elingbeek <elingbeek@digitalriver.com>
2016-10-05 21:23:33 -0500

    app update

--- app/app.js
+++ app/app.js
@@ -2,4 +2,5 @@ require('angular')
 var MainController = require('./controllers/MainController')
 
 var app = angular.module('app', [])
+
 app.controller('MainController', ['$scope', MainController])
